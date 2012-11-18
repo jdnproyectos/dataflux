b83507941662952d843b70ee12cd7b7d64c53d8f
fbruges <fbruges@gmail.com>
2012-11-17 21:40:12 -0500

    bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dataflux::Application.config.secret_token = '33ec881cda4e36d4099b8d71b1ded93562c8d09060826888dd347294bea86112dad2498306cd32e506411f11318df20b7435fd1a4150eb6ea79c0e6279653fbc'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Dataflux::Application.routes.draw do
+
+  root :to => "home#index"
+
+end
