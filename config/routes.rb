5401331d29035fcf65b2e7c7824cee139908c380
fbruges <fbruges@gmail.com>
2014-08-12 16:44:51 -0500

    underscores => hyphens

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Dataflux::Application.routes.draw do
 
   root :to => "home#index"
 
-  match "enlaces_de_interes" => "enlaces#index", :as=>"enlaces"
-  match "soporte_y_descargas" => "soporte#index", :as=>"soporte"
+  match "enlaces-de-interes" => "enlaces#index", :as=>"enlaces"
+  match "soporte-y-descargas" => "soporte#index", :as=>"soporte"
   match "contacto" => "contacto#index"
-  match "contacto_submit" => "contacto#submit"
+  match "contacto-submit" => "contacto#submit"
 
-  match "quienes_somos" => "acerca_de#index", :as=>"acerca_de"
-  match "servicios_web" => "servicios_web#index", :as=>"web"
-  match "soluciones_cloud" => "soluciones_cloud#index", :as=>"cloud"
-  match "proyectos_tic" => "proyectos_tic#index", :as=>"tic"
+  match "quienes-somos" => "acerca_de#index", :as=>"acerca_de"
+  match "servicios-web" => "servicios_web#index", :as=>"web"
+  match "soluciones-cloud" => "soluciones_cloud#index", :as=>"cloud"
+  match "proyectos-tic" => "proyectos_tic#index", :as=>"tic"
 
   match "sitemap" => "sitemap#index"
 
